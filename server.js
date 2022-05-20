032c9280b2287c5be5eb32b99260d7d8427fac35
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-05-20 15:36:09 -0500

    connected heroku and created all requests on server.js

--- server.js
+++ server.js
@@ -4,16 +4,38 @@ const path = require('path');
 const PORT = process.env.PORT || 3001;
 const data = require('./db/db.json');
 
+// Parses
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+app.use(express.static('public'));
+
+function createNewNote(body, notesArray) {
+    console.log(body);
+    const note = body;
+    notesArray.push(note);
+    return note;
+};
+
+
 // API Routes
 app.get("/api/notes", (req, res) => {
     res.json(data);
 });
 
 app.post("/api/notes", (req, res) => {
-    let results = data;
-  console.log(req.query)
-  res.json(results);
+    console.log(req.body);
+    // set id based on what the next index of the array will be
+    req.body.id = data.length.toString();
+    const note = createNewNote(req.body, data);
+    res.json(note);
 });
+
+// app.delete("/api/notes/:id", (req, res) => {
+//     res.json(data);
+// });
 
 // HTML Routes
 app.get("/", (req, res) => {
