60f01b496dae85332ce1348a807fba729010d774
Omar Wright <98981985+omarwright07@users.noreply.github.com>
2022-05-21 01:45:12 -0500

    finished everything but the filter for the delete request

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
-const data = require('./db/db.json');
+const { notes } = require('./db/db.json');
+const uniqid = require('uniqid');
 
 // Parses
 // parse incoming string or array data
@@ -16,26 +18,39 @@ function createNewNote(body, notesArray) {
     console.log(body);
     const note = body;
     notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
     return note;
 };
 
+function filterOutNote(query, notesArray) {
+    let filteredResults = notesArray;
+    filteredResults = filteredResults.filter(
+        note => note.id !== query);
+    return filteredResults;
+};
 
 // API Routes
 app.get("/api/notes", (req, res) => {
-    res.json(data);
+    res.json(notes);
 });
 
 app.post("/api/notes", (req, res) => {
     console.log(req.body);
     // set id based on what the next index of the array will be
-    req.body.id = data.length.toString();
-    const note = createNewNote(req.body, data);
+    req.body.id = uniqid();
+    const note = createNewNote(req.body, notes);
     res.json(note);
 });
 
-// app.delete("/api/notes/:id", (req, res) => {
-//     res.json(data);
-// });
+app.delete("/api/notes/:id", (req, res) => {
+    console.log(notes);
+    console.log(req.params.id);
+    console.log(filterOutNote(req.params.id, notes));
+    // res.json();
+});
 
 // HTML Routes
 app.get("/", (req, res) => {
@@ -51,5 +66,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Note-Taker API Server now on port ${PORT}!`);
+    console.log(`Note-Taker API Server now on port ${PORT}! http://localhost:3001`);
 });
